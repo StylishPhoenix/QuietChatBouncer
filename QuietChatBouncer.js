373005982a10a380b5b76453bc9aa83ac1ac37d1
StylishPhoenix <an007567@gmail.com>
2023-05-30 08:22:45 -0500

    Create QuietChatBouncer.js

---
+++ QuietChatBouncer.js
@@ -0,0 +1,52 @@
+const { Client, Intents, MessageActionRow, MessageButton, CommandInteractionOptionResolver } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
+const { token, quietChatRules, guildId, quietChatId } = require(`./config.json`);
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'asktojoin') {
+        if (!interaction.member.voice.channel) {
+            return interaction.reply("You must be in a voice channel to use this command!");
+        }
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('agree')
+                    .setLabel('I Agree')
+                    .setStyle('SUCCESS'),
+                new ButtonBuilder()
+                    .setCustomId('disagree')
+                    .setLabel('I Do Not Agree')
+                    .setStyle('DANGER'),
+            );
+
+        await interaction.reply({ content: quietChatRules, components: [row], ephemeral: true });
+    }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isButton()) return;
+    if (!interaction.inGuild()) return;
+
+    if (interaction.customId === 'agree') {
+        try {
+             const quietChannel = interaction.guild.channels.cache.get(quietChatId);
+            await interaction.member.voice.setChannel(quietChannel);
+            await interaction.reply({ content: 'You have been moved to the quiet channel.', ephemeral: true });
+        } catch (err) {
+            console.error(err);
+        }
+    } else if (interaction.customId === 'disagree') {
+        await interaction.reply({ content: 'You have chosen not to join the quiet channel.', ephemeral: true });
+    }
+});
+
+client.login('your-bot-token');
